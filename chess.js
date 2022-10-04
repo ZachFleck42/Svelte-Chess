ebaffbd2ec2111c00cfb5d507910256ba122f867
Zachary Fleckenstein <92104124+ZachFleck42@users.noreply.github.com>
2022-10-04 19:35:06 -0400

    Refactors piece generation again again and adds initialBoard

--- chess.js
+++ chess.js
@@ -1,51 +1,62 @@
 let gamePieces = []
 
-function createNewGamePiece(name, color, notation, startPos) {
+function createNewGamePiece(abbreviation, name, color, notation, startPos) {
     piece = {
+        abbreviation: abbreviation,
         name: name,
         color: color,
         notation: notation,
         startPos: startPos,
-        captured: false,
     }
 
     gamePieces.push(piece);
     return piece;
 }
 
-const BKR = createNewGamePiece('Black\'s Kingside Rook', 'Black', 'R', 'a8');
-const BKK = createNewGamePiece('Black\'s Kingside Knight', 'Black', 'N', 'b8');
-const BKB = createNewGamePiece('Black\'s Kingside Bishop', 'Black', 'B', 'c8');
-const BQ = createNewGamePiece('Black\'s Queen', 'Black', 'Q', 'd8');
-const BK = createNewGamePiece('Black\'s King', 'Black', 'K', 'e8');
-const BQB = createNewGamePiece('Black\'s Queenside Bishop', 'Black', 'B', 'f8');
-const BQK = createNewGamePiece('Black\'s Queenside Knight', 'Black', 'N', 'g8');
-const BQR = createNewGamePiece('Black\'s Queenside Rook', 'Black', 'R', 'h8');
-const BPA = createNewGamePiece('Black\'s A Pawn', 'Black', 'P', 'a7');
-const BPB = createNewGamePiece('Black\'s B Pawn', 'Black', 'P', 'b7');
-const BPC = createNewGamePiece('Black\'s C Pawn', 'Black', 'P', 'c7');
-const BPD = createNewGamePiece('Black\'s D Pawn', 'Black', 'P', 'd7');
-const BPE = createNewGamePiece('Black\'s E Pawn', 'Black', 'P', 'e7');
-const BPF = createNewGamePiece('Black\'s F Pawn', 'Black', 'P', 'f7');
-const BPG = createNewGamePiece('Black\'s G Pawn', 'Black', 'P', 'g7');
-const BPH = createNewGamePiece('Black\'s H Pawn', 'Black', 'P', 'h7');
+const BKR = createNewGamePiece('BKR', 'Black\'s Kingside Rook', 'Black', 'R', 'a8');
+const BKK = createNewGamePiece('BKK', 'Black\'s Kingside Knight', 'Black', 'N', 'b8');
+const BKB = createNewGamePiece('BKB', 'Black\'s Kingside Bishop', 'Black', 'B', 'c8');
+const BQz = createNewGamePiece('BQz', 'Black\'s Queen', 'Black', 'Q', 'd8');
+const BKz = createNewGamePiece('BKz', 'Black\'s King', 'Black', 'K', 'e8');
+const BQB = createNewGamePiece('BQB', 'Black\'s Queenside Bishop', 'Black', 'B', 'f8');
+const BQK = createNewGamePiece('BQK', 'Black\'s Queenside Knight', 'Black', 'N', 'g8');
+const BQR = createNewGamePiece('BQR', 'Black\'s Queenside Rook', 'Black', 'R', 'h8');
+const BPA = createNewGamePiece('BPA', 'Black\'s A Pawn', 'Black', 'P', 'a7');
+const BPB = createNewGamePiece('BPB', 'Black\'s B Pawn', 'Black', 'P', 'b7');
+const BPC = createNewGamePiece('BPC', 'Black\'s C Pawn', 'Black', 'P', 'c7');
+const BPD = createNewGamePiece('BPD', 'Black\'s D Pawn', 'Black', 'P', 'd7');
+const BPE = createNewGamePiece('BPE', 'Black\'s E Pawn', 'Black', 'P', 'e7');
+const BPF = createNewGamePiece('BPF', 'Black\'s F Pawn', 'Black', 'P', 'f7');
+const BPG = createNewGamePiece('BPG', 'Black\'s G Pawn', 'Black', 'P', 'g7');
+const BPH = createNewGamePiece('BPH', 'Black\'s H Pawn', 'Black', 'P', 'h7');
 
-const WKR = createNewGamePiece('White\'s Kingside Rook', 'White', 'R', 'a1');
-const WKK = createNewGamePiece('White\'s Kingside Knight', 'White', 'N', 'b1');
-const WKB = createNewGamePiece('White\'s Kingside Bishop', 'White', 'B', 'c1');
-const WQ = createNewGamePiece('White\'s Queen', 'White', 'Q', 'd1');
-const WK = createNewGamePiece('White\'s King', 'White', 'K', 'e1');
-const WQB = createNewGamePiece('White\'s Queenside Bishop', 'White', 'B', 'f1');
-const WQK = createNewGamePiece('White\'s Queenside Knight', 'White', 'N', 'g1');
-const WQR = createNewGamePiece('White\'s Queenside Rook', 'White', 'R', 'h1');
-const WPA = createNewGamePiece('White\'s A Pawn', 'White', 'P', 'a2');
-const WPB = createNewGamePiece('White\'s B Pawn', 'White', 'P', 'b2');
-const WPC = createNewGamePiece('White\'s C Pawn', 'White', 'P', 'c2');
-const WPD = createNewGamePiece('White\'s D Pawn', 'White', 'P', 'd2');
-const WPE = createNewGamePiece('White\'s E Pawn', 'White', 'P', 'e2');
-const WPF = createNewGamePiece('White\'s F Pawn', 'White', 'P', 'f2');
-const WPG = createNewGamePiece('White\'s G Pawn', 'White', 'P', 'g2');
-const WPH = createNewGamePiece('White\'s H Pawn', 'White', 'P', 'h2');
+const WKR = createNewGamePiece('WKR', 'White\'s Kingside Rook', 'White', 'R', 'a1');
+const WKK = createNewGamePiece('WKK', 'White\'s Kingside Knight', 'White', 'N', 'b1');
+const WKB = createNewGamePiece('WKB', 'White\'s Kingside Bishop', 'White', 'B', 'c1');
+const WQz = createNewGamePiece('WQz', 'White\'s Queen', 'White', 'Q', 'd1');
+const WKz = createNewGamePiece('WKz', 'White\'s King', 'White', 'K', 'e1');
+const WQB = createNewGamePiece('WQB', 'White\'s Queenside Bishop', 'White', 'B', 'f1');
+const WQK = createNewGamePiece('WQK', 'White\'s Queenside Knight', 'White', 'N', 'g1');
+const WQR = createNewGamePiece('WQR', 'White\'s Queenside Rook', 'White', 'R', 'h1');
+const WPA = createNewGamePiece('WPA', 'White\'s A Pawn', 'White', 'P', 'a2');
+const WPB = createNewGamePiece('WPB', 'White\'s B Pawn', 'White', 'P', 'b2');
+const WPC = createNewGamePiece('WPC', 'White\'s C Pawn', 'White', 'P', 'c2');
+const WPD = createNewGamePiece('WPD', 'White\'s D Pawn', 'White', 'P', 'd2');
+const WPE = createNewGamePiece('WPE', 'White\'s E Pawn', 'White', 'P', 'e2');
+const WPF = createNewGamePiece('WPF', 'White\'s F Pawn', 'White', 'P', 'f2');
+const WPG = createNewGamePiece('WPG', 'White\'s G Pawn', 'White', 'P', 'g2');
+const WPH = createNewGamePiece('WPH', 'White\'s H Pawn', 'White', 'P', 'h2');              
+
+const emptyBoard = [
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']
+];
 
 const boardSquares = [
     ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8'],
@@ -58,7 +69,7 @@ const boardSquares = [
     ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1']
 ];
 
-function getCoordinates(square) {
+function getCoordinate(square) {
     for (let i = 0; i < boardSquares.length; i++) {
         for (let j = 0; j < boardSquares[i].length; j++) {
             if (boardSquares[i][j] === square) {
@@ -72,5 +83,20 @@ function getSquare(coordinate) {
     return boardSquares[coordinate[0]][coordinate[1]];
 }
 
-BKR.captured = true;
-console.log(getCoordinates(BKR.startPos));
+let initialBoard = boardSquares.map(
+    (row) => {
+        return row.map(
+            (square) => {
+                for (let i = 0; i < gamePieces.length; i++) {
+                    if (gamePieces[i].startPos === square) {
+                        return gamePieces[i].abbreviation
+                    }
+                }
+                return 'x';
+            }
+        )
+    }
+)
+
+
+console.log(initialBoard);
