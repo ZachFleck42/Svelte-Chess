e165e62d303691160f94b0670ffaf5d13cd70aa1
Zachary Fleckenstein <92104124+ZachFleck42@users.noreply.github.com>
2022-10-04 15:01:27 -0400

    Refactors piece generation

--- chess.js
+++ chess.js
@@ -31,45 +31,49 @@ class GamePiece {
     }
 }
 
+let gamePieces = [];
+
 function newPiece(shortName, fullName, color, notation, startPos) {
-    
+    p = new GamePiece(shortName, fullName, color, notation, startPos);
+    gamePieces.push(p);
+    return p;
 }
 
-gamePieces.newPiece('BKR', 'Black\'s Kingside Rook', 'Black', 'R', 'a8');
-gamePieces.newPiece('BKK', 'Black\'s Kingside Knight', 'Black', 'N', 'b8');
-gamePieces.newPiece('BKB', 'Black\'s Kingside Bishop', 'Black', 'B', 'c8');
-gamePieces.newPiece('BQ ', 'Black\'s Queen', 'Black', 'Q', 'd8');
-gamePieces.newPiece('BK ', 'Black\'s King', 'Black', 'K', 'e8');
-gamePieces.newPiece('BQB', 'Black\'s Queenside Bishop', 'Black', 'B', 'f8');
-gamePieces.newPiece('BQK', 'Black\'s Queenside Knight', 'Black', 'N', 'g8');
-gamePieces.newPiece('BQR', 'Black\'s Queenside Rook', 'Black', 'R', 'h8');
-gamePieces.newPiece('BPA', 'Black\'s A Pawn', 'Black', 'P', 'a7');
-gamePieces.newPiece('BPB', 'Black\'s B Pawn', 'Black', 'P', 'b7');
-gamePieces.newPiece('BPC', 'Black\'s C Pawn', 'Black', 'P', 'c7');
-gamePieces.newPiece('BPD', 'Black\'s D Pawn', 'Black', 'P', 'd7');
-gamePieces.newPiece('BPE', 'Black\'s E Pawn', 'Black', 'P', 'e7');
-gamePieces.newPiece('BPF', 'Black\'s F Pawn', 'Black', 'P', 'f7');
-gamePieces.newPiece('BPG', 'Black\'s G Pawn', 'Black', 'P', 'g7');
-gamePieces.newPiece('BPH', 'Black\'s H Pawn', 'Black', 'P', 'h7');
+newPiece('BKR', 'Black\'s Kingside Rook', 'Black', 'R', 'a8');
+newPiece('BKK', 'Black\'s Kingside Knight', 'Black', 'N', 'b8');
+newPiece('BKB', 'Black\'s Kingside Bishop', 'Black', 'B', 'c8');
+newPiece('BQ ', 'Black\'s Queen', 'Black', 'Q', 'd8');
+newPiece('BK ', 'Black\'s King', 'Black', 'K', 'e8');
+newPiece('BQB', 'Black\'s Queenside Bishop', 'Black', 'B', 'f8');
+newPiece('BQK', 'Black\'s Queenside Knight', 'Black', 'N', 'g8');
+newPiece('BQR', 'Black\'s Queenside Rook', 'Black', 'R', 'h8');
+newPiece('BPA', 'Black\'s A Pawn', 'Black', 'P', 'a7');
+newPiece('BPB', 'Black\'s B Pawn', 'Black', 'P', 'b7');
+newPiece('BPC', 'Black\'s C Pawn', 'Black', 'P', 'c7');
+newPiece('BPD', 'Black\'s D Pawn', 'Black', 'P', 'd7');
+newPiece('BPE', 'Black\'s E Pawn', 'Black', 'P', 'e7');
+newPiece('BPF', 'Black\'s F Pawn', 'Black', 'P', 'f7');
+newPiece('BPG', 'Black\'s G Pawn', 'Black', 'P', 'g7');
+newPiece('BPH', 'Black\'s H Pawn', 'Black', 'P', 'h7');
 
-gamePieces.newPiece('WKR', 'White\'s Kingside Rook', 'White', 'R', 'a1');
-gamePieces.newPiece('WKK', 'White\'s Kingside Knight', 'White', 'N', 'b1');
-gamePieces.newPiece('WKB', 'White\'s Kingside Bishop', 'White', 'B', 'c1');
-gamePieces.newPiece('WQ ', 'White\'s Queen', 'White', 'Q', 'd1');
-gamePieces.newPiece('WK ', 'White\'s King', 'White', 'K', 'e1');
-gamePieces.newPiece('WQB', 'White\'s Queenside Bishop', 'White', 'B', 'f1');
-gamePieces.newPiece('WQK', 'White\'s Queenside Knight', 'White', 'N', 'g1');
-gamePieces.newPiece('WQR', 'White\'s Queenside Rook', 'White', 'R', 'h1');
-gamePieces.newPiece('WPA', 'White\'s A Pawn', 'White', 'P', 'a2');
-gamePieces.newPiece('WPB', 'White\'s B Pawn', 'White', 'P', 'b2');
-gamePieces.newPiece('WPC', 'White\'s C Pawn', 'White', 'P', 'c2');
-gamePieces.newPiece('WPD', 'White\'s D Pawn', 'White', 'P', 'd2');
-gamePieces.newPiece('WPE', 'White\'s E Pawn', 'White', 'P', 'e2');
-gamePieces.newPiece('WPF', 'White\'s F Pawn', 'White', 'P', 'f2');
-gamePieces.newPiece('WPG', 'White\'s G Pawn', 'White', 'P', 'g2');
-gamePieces.newPiece('WPH', 'White\'s H Pawn', 'White', 'P', 'h2');
+newPiece('WKR', 'White\'s Kingside Rook', 'White', 'R', 'a1');
+newPiece('WKK', 'White\'s Kingside Knight', 'White', 'N', 'b1');
+newPiece('WKB', 'White\'s Kingside Bishop', 'White', 'B', 'c1');
+newPiece('WQ ', 'White\'s Queen', 'White', 'Q', 'd1');
+newPiece('WK ', 'White\'s King', 'White', 'K', 'e1');
+newPiece('WQB', 'White\'s Queenside Bishop', 'White', 'B', 'f1');
+newPiece('WQK', 'White\'s Queenside Knight', 'White', 'N', 'g1');
+newPiece('WQR', 'White\'s Queenside Rook', 'White', 'R', 'h1');
+newPiece('WPA', 'White\'s A Pawn', 'White', 'P', 'a2');
+newPiece('WPB', 'White\'s B Pawn', 'White', 'P', 'b2');
+newPiece('WPC', 'White\'s C Pawn', 'White', 'P', 'c2');
+newPiece('WPD', 'White\'s D Pawn', 'White', 'P', 'd2');
+newPiece('WPE', 'White\'s E Pawn', 'White', 'P', 'e2');
+newPiece('WPF', 'White\'s F Pawn', 'White', 'P', 'f2');
+newPiece('WPG', 'White\'s G Pawn', 'White', 'P', 'g2');
+newPiece('WPH', 'White\'s H Pawn', 'White', 'P', 'h2');
 
-
+console.log(gamePieces);
 
 const boardSquares = [
     ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8'],
@@ -95,5 +99,3 @@ function getCoordinate(square) {
 function getSquare(coordinate) {
     return boardSquares[coordinate[0]][coordinate[1]];
 }
-
-console.log(gamePieces.allPieces[0]);
