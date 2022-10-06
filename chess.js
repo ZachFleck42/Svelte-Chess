818b3ec621934162471c642ff4b1249c0bbb0e71
Zachary Fleckenstein <92104124+ZachFleck42@users.noreply.github.com>
2022-10-06 13:03:25 -0400

    Begins implementing pawn movement logic

--- chess.js
+++ chess.js
@@ -14,12 +14,12 @@ function createNewGamePiece(abbreviation, name, color, notation, startPos) {
 }
 
 const BKR = createNewGamePiece('BKR', 'Black\'s Kingside Rook', 'Black', 'R', 'a8');
-const BKK = createNewGamePiece('BKN', 'Black\'s Kingside Knight', 'Black', 'N', 'b8');
+const BKN = createNewGamePiece('BKN', 'Black\'s Kingside Knight', 'Black', 'N', 'b8');
 const BKB = createNewGamePiece('BKB', 'Black\'s Kingside Bishop', 'Black', 'B', 'c8');
-const BQz = createNewGamePiece('BQQ', 'Black\'s Queen', 'Black', 'Q', 'd8');
-const BKz = createNewGamePiece('BKK', 'Black\'s King', 'Black', 'K', 'e8');
+const BQQ = createNewGamePiece('BQQ', 'Black\'s Queen', 'Black', 'Q', 'd8');
+const BKK = createNewGamePiece('BKK', 'Black\'s King', 'Black', 'K', 'e8');
 const BQB = createNewGamePiece('BQB', 'Black\'s Queenside Bishop', 'Black', 'B', 'f8');
-const BQK = createNewGamePiece('BQN', 'Black\'s Queenside Knight', 'Black', 'N', 'g8');
+const BQN = createNewGamePiece('BQN', 'Black\'s Queenside Knight', 'Black', 'N', 'g8');
 const BQR = createNewGamePiece('BQR', 'Black\'s Queenside Rook', 'Black', 'R', 'h8');
 const BPA = createNewGamePiece('BPA', 'Black\'s A Pawn', 'Black', 'P', 'a7');
 const BPB = createNewGamePiece('BPB', 'Black\'s B Pawn', 'Black', 'P', 'b7');
@@ -31,12 +31,12 @@ const BPG = createNewGamePiece('BPG', 'Black\'s G Pawn', 'Black', 'P', 'g7');
 const BPH = createNewGamePiece('BPH', 'Black\'s H Pawn', 'Black', 'P', 'h7');
 
 const WKR = createNewGamePiece('WKR', 'White\'s Kingside Rook', 'White', 'R', 'a1');
-const WKK = createNewGamePiece('WKN', 'White\'s Kingside Knight', 'White', 'N', 'b1');
+const WKN = createNewGamePiece('WKN', 'White\'s Kingside Knight', 'White', 'N', 'b1');
 const WKB = createNewGamePiece('WKB', 'White\'s Kingside Bishop', 'White', 'B', 'c1');
-const WQz = createNewGamePiece('WQQ', 'White\'s Queen', 'White', 'Q', 'd1');
-const WKz = createNewGamePiece('WKK', 'White\'s King', 'White', 'K', 'e1');
+const WQQ = createNewGamePiece('WQQ', 'White\'s Queen', 'White', 'Q', 'd1');
+const WKK = createNewGamePiece('WKK', 'White\'s King', 'White', 'K', 'e1');
 const WQB = createNewGamePiece('WQB', 'White\'s Queenside Bishop', 'White', 'B', 'f1');
-const WQK = createNewGamePiece('WQN', 'White\'s Queenside Knight', 'White', 'N', 'g1');
+const WQN = createNewGamePiece('WQN', 'White\'s Queenside Knight', 'White', 'N', 'g1');
 const WQR = createNewGamePiece('WQR', 'White\'s Queenside Rook', 'White', 'R', 'h1');
 const WPA = createNewGamePiece('WPA', 'White\'s A Pawn', 'White', 'P', 'a2');
 const WPB = createNewGamePiece('WPB', 'White\'s B Pawn', 'White', 'P', 'b2');
@@ -73,9 +73,11 @@ let initialBoard = boardSquares.map(
     }
 )
 
-let gameHistory = [initialBoard];
+function getSquare(arrayPos) {
+    return boardSquares[arrayPos[0]][arrayPos[2]];
+}
 
-function getArrayPos(square) {
+function getSquareArrayPos(square) {
     for (let i = 0; i < boardSquares.length; i++) {
         for (let j = 0; j < boardSquares[i].length; j++) {
             if (boardSquares[i][j] === square) {
@@ -84,10 +86,6 @@ function getArrayPos(square) {
         }
     }
     return [-1, -1];
-}
-
-function getSquare(arrayPos) {
-    return boardSquares[arrayPos[0]][arrayPos[2]];
 }
 
 function getPieceArrayPos(gameBoard, gamePiece) {
@@ -101,27 +99,51 @@ function getPieceArrayPos(gameBoard, gamePiece) {
 }
 
 function checkMoveValidity(gameBoard, pieceMoved, destinationSquare) {
+    // Verify destinationSquare is on the board
     if (!('abcdefgh'.includes(destinationSquare[0])) || destinationSquare[1] < 1 || destinationSquare[1] > 8) {
-        return -1
+        return 1;
     }
 
+    let piecePos = getPieceArrayPos(gameBoard, pieceMoved)
+    let squarePos = getSquareArrayPos(destinationSquare)
 
-    if (pieceMoved[1] = 'P') {
+    // Verify piece is not moving to square it's already on
+    if (piecePos === squarePos) {
+        return 2;
+    }
+
+    // Pawn logic
+    if (pieceMoved[1] === 'P') {
+        if (pieceMoved[0] === 'W') {
+            if (squarePos[0] - piecePos[0] !== 1) {
+                console.log(`piecePos:  ${piecePos}`);
+                console.log(`squarePos: ${squarePos}`);
+                console.log((piecePos[0] - squarePos[0]));
+                // Allow pawn to move two spaces forward if it's in starting position
+                // and no other pieces are in its path
+                if ((piecePos[0] - squarePos[0] === 2) && (piecePos[0] === '6') && (squarePos[2] === piecePos[2]) && (gameBoard[squarePos[0]][squarePos[2]] === 'x') && (gameBoard[piecePos[0] - 1][piecePos[2]] === 'x')) {
+                    return 'YES';
+                }
+            }
+        }
+        else if (pieceMoved[0] === 'B') {
+
+        }
+    }
+    // Bishop logic
+    else if (pieceMoved[1] === 'B') {
 
     }
-    else if (pieceMoved[1] = 'B') {
+    else if (pieceMoved[1] === 'R') {
 
     }
-    else if (pieceMoved[1] = 'R') {
+    else if (pieceMoved[1] === 'N') {
 
     }
-    else if (pieceMoved[1] = 'N') {
+    else if (pieceMoved[1] === 'Q') {
 
     }
-    else if (pieceMoved[1] = 'Q') {
-
-    }
-    else if (pieceMoved[1] = 'K') {
+    else if (pieceMoved[1] === 'K') {
 
     }
 }
@@ -135,7 +157,7 @@ function getNewBoardStandard(oldBoard, pieceMoved, destinationSquare) {
                         if (square === pieceMoved) {
                             return 'x';
                         }
-                        else if (getArrayPos(destinationSquare) === `${indexOfRowInBoard},${indexOfSquareInRow}`) {
+                        else if (getSquareArrayPos(destinationSquare) === `${indexOfRowInBoard},${indexOfSquareInRow}`) {
                             return pieceMoved;
                         }
                         else {
@@ -158,6 +180,8 @@ function getNewBoardEnPassant() {
 
 }
 
+let gameHistory = [initialBoard];
+
 // Main game loop; runs until checkmate
 
     // Player picks a piece to move
@@ -171,4 +195,6 @@ function getNewBoardEnPassant() {
         // If check, note for next move
         // If checkmate, end game
 
-console.log(getSquare(getPieceArrayPos(initialBoard, 'BPB')));
+console.log(`arrayPos WPA: ${getPieceArrayPos(initialBoard, 'WPA')}`)
+console.log(`arrayPos destinationSquare: ${getSquareArrayPos('a4')}`)
+console.log(checkMoveValidity(initialBoard, 'WPA', 'a4'));
