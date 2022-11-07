251177997873bed4eee3ce2ff4a43fef46634be1
Zachary Fleckenstein <92104124+ZachFleck42@users.noreply.github.com>
2022-11-07 14:30:01 -0500

    Matching conditions to production

---
+++ prettier.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+	plugins: [
+		require('./node_modules/prettier-plugin-tailwindcss'),
+		require('./node_modules/@trivago/prettier-plugin-sort-imports'),
+	],
+	tailwindConfig: './tailwind.config.cjs',
+	arrowParens: 'always',
+	bracketSameLine: true,
+	bracketSpacing: false,
+	printWidth: 120,
+	proseWrap: 'always',
+	semi: true,
+	trailingComma: 'all',
+	singleQuote: true,
+	tabWidth: 2,
+	useTabs: true,
+	importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	// https://github.com/sveltejs/prettier-plugin-svelte/issues/155#issuecomment-831166730
+	overrides: [
+		{
+			files: '*.svelte',
+			options: {
+				parser: 'svelte',
+			},
+		},
+	],
+};
