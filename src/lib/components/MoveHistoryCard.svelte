9467cfc7722991d752e5a364de02665a9c43b12e
Zachary Fleckenstein <ZachFleck42@Gmail.com>
2023-04-05 16:29:29 -0400

    Changes spacing on MoveHistoryCard

--- src/lib/components/MoveHistoryCard.svelte
+++ src/lib/components/MoveHistoryCard.svelte
@@ -6,7 +6,7 @@
   {#each moveHistory as move, i}
     {#if i % 2 === 0}
       <span class="turn-no">{Math.floor(i / 2 + 1)}.</span>
-      <span class="white-move">{move[0]} to {move[1]}</span>
+      <span class="white-move">{move[0]} to {move[1]}&nbsp; &nbsp; &nbsp;</span>
     {:else}
       <span class="black-move">{move[0]} to {move[1]}</span>
       <br />
